ca454dd05a9691af18f084b486a19ce0583441dc
yoptohlejepeta <peta.kotlan@seznam.cz>
2025-02-07 21:08:56 +0100

    primitive_types done

--- exercises/04_primitive_types/primitive_types3.rs
+++ exercises/04_primitive_types/primitive_types3.rs
@@ -1,6 +1,8 @@
 fn main() {
     // TODO: Create an array called `a` with at least 100 elements in it.
     // let a = ???
+    
+    let a: Vec<i64> = (0..100).collect();
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
